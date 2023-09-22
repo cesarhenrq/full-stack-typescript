b87ed247918392f46f1176a8b13ec2e7178a507b
cesarhenrq <cesaarhvb@gmail.com>
2023-09-22 12:05:57 -0300

    9.1 Body mass index

---
+++ partb/bmiCalculator.ts
@@ -0,0 +1,19 @@
+const calculateBmi = (height: number, weight: number): string => {
+  const bmi = weight / (height / 100) ** 2;
+
+  switch (true) {
+    case bmi < 18.5:
+      return "Underweight";
+    case bmi < 25:
+      return "Normal (healthy weight)";
+    case bmi < 30:
+      return "Overweight";
+    default:
+      return "Obese";
+  }
+};
+
+const height = Number(process.argv[2]);
+const weight = Number(process.argv[3]);
+
+console.log(calculateBmi(height, weight));
