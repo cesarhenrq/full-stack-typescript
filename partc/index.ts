7a0169c5fccfe85259ddddcd86fba361877b4303
cesarhenrq <cesaarhvb@gmail.com>
2023-09-24 19:41:14 -0300

    9.9: Patientor backend, step2

--- partc/index.ts
+++ partc/index.ts
@@ -1,13 +1,18 @@
-import express from "express";
+import express, { Express } from "express";
 
-const app = express();
+import cors from "cors";
+
+const app: Express = express();
+
+app.use(cors());
+
 app.use(express.json());
 
 app.get("/api/ping", (_req, res) => {
   res.send("pong");
 });
 
-const PORT = 3003;
+const PORT = 3001;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
