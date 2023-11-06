2cd3c6ebe545d96c035e1a0c2bfcf09f92ef9db1
cesarhenrq <cesaarhvb@gmail.com>
2023-11-06 19:13:25 -0300

    refactor: content component to use Part component

--- partd/src/components/content/index.tsx
+++ partd/src/components/content/index.tsx
@@ -1,12 +1,12 @@
+import Part from "../part";
+
 import IProps from "./props";
 
 const Content = ({ courseParts }: IProps) => {
   return (
     <>
       {courseParts.map((part, index) => (
-        <p key={index}>
-          {part.name} {part.exerciseCount}
-        </p>
+        <Part coursePart={part} key={index} />
       ))}
     </>
   );
