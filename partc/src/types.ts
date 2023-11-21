961af4f0e448429ead56f35fcdb9a3d0c35b998a
César Henrique <cesaarhvb@gmail.com>
2023-11-20 22:39:14 -0300

    feat: add new entry types to types.ts

--- partc/src/types.ts
+++ partc/src/types.ts
@@ -1,3 +1,40 @@
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis["code"]>;
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3,
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: {
+    startDate: string;
+    endDate: string;
+  };
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: {
+    date: string;
+    criteria: string;
+  };
+}
+
 export enum Gender {
   male = "male",
   female = "female",
@@ -11,7 +48,10 @@ export interface Diagnosis {
 }
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {}
+export type Entry =
+  | HealthCheckEntry
+  | OccupationalHealthcareEntry
+  | HospitalEntry;
 
 export interface Patient {
   id: string;
