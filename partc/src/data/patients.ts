0aed0dd5afdf5e57665a2022c7d2f6be0be42fa8
César Henrique <cesaarhvb@gmail.com>
2023-11-21 01:18:15 -0300

    feat: add Patient type to data array

--- partc/src/data/patients.ts
+++ partc/src/data/patients.ts
@@ -1,6 +1,6 @@
-import { Gender } from "../types";
+import { Patient, Gender } from "../types";
 
-const data = [
+const data: Patient[] = [
   {
     id: "d2773336-f723-11e9-8f0b-362b9e155667",
     name: "John McClane",
