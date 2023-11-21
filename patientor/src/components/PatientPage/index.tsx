c00762d9b50a5249347219be93ae4a907ee039d7
César Henrique <cesaarhvb@gmail.com>
2023-11-21 03:27:20 -0300

    feat: add new entry form and functionality

--- patientor/src/components/PatientPage/index.tsx
+++ patientor/src/components/PatientPage/index.tsx
@@ -4,17 +4,21 @@ import { useParams } from "react-router-dom";
 
 import GenderIcon from "../GenderIcon";
 import BaseEntry from "../BaseEntry";
+import AddEntryForm from "../AddEntryForm";
 
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, Button } from "@mui/material";
 
-import { Diagnosis, Patient } from "../../types";
+import { Diagnosis, NewEntryFormValues, Patient } from "../../types";
 
 import patientService from "../../services/patients";
 import diagnosisService from "../../services/diagnosis";
+import { isAxiosError } from "axios";
 
 const PatientPage = () => {
   const [patient, setPatient] = React.useState<Patient | undefined>();
   const [diagnoses, setDiagnoses] = React.useState<Diagnosis[]>([]);
+  const [isOpen, setIsOpen] = React.useState<boolean>(false);
+  const [error, setError] = React.useState<string | undefined>();
 
   const { id } = useParams<{ id: string }>();
 
@@ -40,6 +44,35 @@ const PatientPage = () => {
     fetchDiagnoses();
   }, []);
 
+  const handleOpen = () => {
+    setIsOpen(true);
+  };
+
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+
+  const handleSubmit = async (newEntryFormValues: NewEntryFormValues) => {
+    try {
+      const entry = await patientService.createEntry(
+        id as string,
+        newEntryFormValues
+      );
+      if (!patient) return;
+      setPatient({ ...patient, entries: patient?.entries.concat(entry) });
+    } catch (e) {
+      if (isAxiosError(e)) {
+        setError(e.response?.data);
+      } else {
+        throw e;
+      }
+
+      setTimeout(() => {
+        setError(undefined);
+      }, 5000);
+    }
+  };
+
   return (
     <Box style={{ marginTop: "2em" }}>
       <Box style={{ display: "flex", alignItems: "center" }}>
@@ -56,6 +89,21 @@ const PatientPage = () => {
         <Typography style={{ marginBottom: "0.5em" }}>
           <strong>Date of birth:</strong> {patient?.dateOfBirth}
         </Typography>
+        <Box>
+          <Button
+            variant='contained'
+            onClick={handleOpen}
+            style={{ display: `${isOpen ? "none" : "block"}` }}
+          >
+            Add New Entry
+          </Button>
+          <AddEntryForm
+            isOpen={isOpen}
+            onClose={handleClose}
+            onSubmit={handleSubmit}
+            error={error}
+          />
+        </Box>
         <Typography variant='h6'>Entries</Typography>
         {patient?.entries.map((entry) => (
           <BaseEntry key={entry.id} entry={entry} diagnoses={diagnoses} />
